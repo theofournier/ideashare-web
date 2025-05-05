6f6b14615ec0bdbdc26e2101b83d6beb72691dbd
theofournier <fournier.theo@gmail.com>
2025-05-05 14:59:22 -0400

    feat: add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,6 +51,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <Button>Shadcn button</Button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
